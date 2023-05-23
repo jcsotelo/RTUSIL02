6fa9ad0b8bcd3dd6ed30862a75651da84ccd489e
juancarlos <jc_sotelo@yahoo.com>
2023-05-22 23:06:13 -0500

    primer commit

---
+++ ValidarGets.cs
@@ -0,0 +1,36 @@
+﻿using TestApi;
+using TestApi.DTOs;
+using Xunit;
+
+namespace UnitTesting.Test
+{
+    public class ValidarGets
+    {
+        [Fact]
+        public void ValidarNombreArchivoPlano()
+        {
+            var validarObjetos = new ValidarObjetos();
+
+            string nombrearchivo = "C:\\JuanCarlos\\USIL\\gistfile1.txt";
+
+            bool isvalid = validarObjetos.NombreArchivotxt(nombrearchivo);
+
+            Assert.True(isvalid);
+
+        }
+
+        [Fact]
+        public void ValidarCapacidadArchivoPLano()
+        {
+            var validarObjetos = new ValidarObjetos();
+
+            //string nombrearchivo = "C:\\JuanCarlos\\USIL\\gistfile1.txt";
+
+            string capacidad = validarObjetos.CapacidadArchivotxt();
+
+            string expectativa = "Vacio";
+
+            Assert.Equal(capacidad, expectativa);
+        }
+    }
+}
